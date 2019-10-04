a3288c28d00a63006436d8afcb40777b1dc24906
Arewic <arevik.antonyan.1998@mail.ru>
2019-10-04 09:46:55 +0300

    Changes

--- Lab_3/Program.cs
+++ Lab_3/Program.cs
@@ -8,8 +8,88 @@ namespace Lab_3
 {
     class Program
     {
+        static double X;
+
         static void Main(string[] args)
         {
+            Fibonacci_Method(Z2);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// В программе производится поиск отрезка (A,B),
+        /// содержащего точки минимума унимодальной функции F(x),
+        /// за N вычислений функции.
+        /// Необходимые числа Фибоначчи вычисляются здесь же.
+        /// </summary>
+        static void Fibonacci_Method(Func<double> Z)
+        {
+            Console.WriteLine("Поиск методом Фибоначчи");
+            int[] F = new int[40];
+            double A, B, E,X1, X2, X3, X4 , F2, F4;
+            Console.WriteLine("Задайте N");
+            int N = int.Parse(Console.ReadLine());
+            F[0] = 1;F[1] = 1;
+            for (int i = 2; i <= N; i++)
+                F[i] = F[i - 1] + F[i - 2];
+            Console.WriteLine("Задайте Epsilon");
+            E = double.Parse(Console.ReadLine());
+            Console.WriteLine("Задайте интервал (A,B)");
+            string[] str = Console.ReadLine().Split(' ');
+            A = double.Parse(str[0]);
+            B = double.Parse(str[1]);
+            X1 = A;
+            X2 = A + ((B - A) * F[N - 1] + E * Math.Pow(-1, N))/ F[N];X3 = B;
+            X = X2;F2 = Z();
+            Console.WriteLine("\tТекущий интервал");
+            int K = 1; Console.WriteLine($"{X1,-20}\t{X3,-20}");
+            while (K <= N)
+            {
+                X4 = X1 - X2 + X3;
+                X = X4; F4 = Z();
+                if (F4 > F2)
+                {
+                    if (X2 < X4)
+                    {
+                        X3 = X4; 
+                    }
+                    else
+                    {
+                        X1 = X4;
+                    }
+                }
+                else
+                {
+                    if (X2 < X4)
+                    {
+                        X1 = X2; X2 = X4; F2 = F4;
+                    }
+                    else
+                    {
+                        X3 = X2; X2 = X4; F2 = F4; 
+                    }
+                }
+                K++;
+                Console.WriteLine($"{X1,-20}\t{X3,-20}");
+            }
+            Console.WriteLine("\tКонечный интервал");
+            Console.WriteLine($"{X1}\t{X3}");
+            Console.WriteLine($"Значение функции равно {F2}");
+        }
+        //Функция f(x)=2∙x^2-e^x.
+        static double Z1()
+        {
+            return 2*Math.Pow(X,2) - Math.Pow(Math.E, X);
+        }
+        //Функция f(x)=x^4-14∙x^3+60∙x^2-70∙x 
+        static double Z2()
+        {
+            return Math.Pow(X, 4) - 14 * Math.Pow(X, 3) + 60 * Math.Pow(X, 2) - 70 * X;
+        }
+        //Функция f(x)=2∙x^2-e^x.
+        static double Z3()
+        {
+            return 2 * Math.Pow(X, 2) + 3*Math.Pow(Math.E, -X);
         }
     }
 }
---
+++ SelfWork_8/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SelfWork_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("a = 0.2");
+            Console.WriteLine("b = 1");
+            Console.WriteLine("e = 0.0001");
+            Console.WriteLine("");
+            Console.WriteLine("Метод дихотомии");
+            DichotomyMethod(0.2,1,0.0001);
+            Console.WriteLine("");
+            Console.WriteLine("Метод Ньютона");
+            NewtonMethod(0.2, 1, 0.0001);
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Метод дихотомии
+        /// </summary>
+        /// <param name="a">начало отрезка</param>
+        /// <param name="b">конец отрезка</param>
+        /// <param name="e">заданная точность</param>
+        static void DichotomyMethod(double a, double b, double e)
+        {
+            int n = 1;
+            double x,x1, x2, f1, f2;
+            double d = (b - a) * e;
+            do
+            {
+                x1 = (b + a - d) / 2;
+                x2 = (b + a + d) / 2;
+                f1 = Fn(x1);
+                f2 = Fn(x2);
+                if (f1 <= f2)
+                    b = x2;
+                else
+                    a = x1;
+                n++;
+            } while ((b - a) / 2 > e);
+            x = (a + b) / 2;
+            Console.WriteLine($"x* = {x} , кол-во итераций = {n}");
+        }
+        /// <summary>
+        /// Метод Ньютона
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <param name="e"></param>
+        public static void NewtonMethod(double a, double b, double e)
+        {
+            double x;
+            int n = 1;
+            do
+            {
+                x = a - Der(a) / DDer(a);
+                a = x;
+                n++;
+            } while (Math.Abs(Der(x)) > e);
+            Console.WriteLine($"x* = {x} , кол-во итераций = {n}");
+        }
+
+        //Функция 
+        static double Fn(double x)
+        {
+            return x * Math.Sin(1/x);
+        }
+        //Производная 
+        static double Der(double x)
+        {
+            return Math.Sin(1 / x)- 1/x * Math.Cos(1 / x); 
+        }
+        //Двойная производная 
+        static double DDer(double x)
+        {
+            return -Math.Sin(1 / x)/Math.Pow(x,3);
+        }
+    }
+}
